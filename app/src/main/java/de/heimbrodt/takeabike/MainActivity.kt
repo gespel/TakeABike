942058f815305b4a9343cfad23072f9605c62ab3
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2022-09-26 05:32:58 +0200

    alles scuffed

--- app/src/main/java/de/heimbrodt/takeabike/MainActivity.kt
+++ app/src/main/java/de/heimbrodt/takeabike/MainActivity.kt
@@ -3,6 +3,7 @@ package de.heimbrodt.takeabike
 import android.location.Location
 import android.os.Bundle
 import android.os.Handler
+import android.os.HandlerThread
 import android.os.Looper
 import android.renderscript.RenderScript.Priority
 import com.google.android.material.snackbar.Snackbar
@@ -24,13 +25,12 @@ import com.google.android.gms.tasks.OnTokenCanceledListener
 import de.heimbrodt.takeabike.databinding.ActivityMainBinding
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var a: Location
     private lateinit var b: Location
 
 
@@ -38,46 +38,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        TakeABike.setTABBinding(binding)
         setContentView(binding.root)
-        var distance = 0.0
-        val mainHandler = Handler(Looper.getMainLooper())
-        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
-            if (location != null) {
-                a = location
-            }
-
-        }
-        mainHandler.post(object : Runnable {
-            override fun run() {
-                fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
-                    override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
-
-                    override fun isCancellationRequested() = false
-                })
-                    .addOnSuccessListener { location: Location? ->
-                        if (location == null)
-                        else {
-                            binding.msTextView.text = "Speed: " + location.distanceTo(a).toString() +" m/s"
-                            binding.latitudeTextView.text = "Latitude: " + location.latitude.toString()
-                            binding.longitudeTextView.text = "Logitude: " + location.longitude.toString()
-                            binding.timeTextView.text = "Time: " + LocalDateTime.now().format(
-                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                            )
-                            distance += location.distanceTo(a)
-                            binding.distanceTextView.text = "Distance: $distance m"
-                            a = location
-                        }
-                    }
-                mainHandler.postDelayed(this, 1000)
-            }
-        })
-
-        //val navController = findNavController(R.id.nav_host_fragment_content_main)
-        //appBarConfiguration = AppBarConfiguration(navController.graph)
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-
+        binding.maxSpeedTextView.text = "0.0"
+        TakeABike.p.setTABBinding(binding)
 
     }
 
--- app/src/main/java/de/heimbrodt/takeabike/TABPlayer.kt
+++ app/src/main/java/de/heimbrodt/takeabike/TABPlayer.kt
@@ -1,5 +1,26 @@
 package de.heimbrodt.takeabike
 
-class TABPlayer {
-    private val level: Int = 1
+import de.heimbrodt.takeabike.databinding.ActivityMainBinding
+
+class TABPlayer() {
+    public var level: Int = 1
+    public var exp: Int = 0
+    private lateinit var binding: ActivityMainBinding
+    init {
+
+    }
+    public fun tickExp(distance: Float) {
+        exp += (distance).toInt()
+        binding.expTextView.text = "Exp: $exp"
+    }
+    private fun updateLevel() {
+        if(exp >= level*100+(level*level*0.5)) {
+            exp -= level*100+(level*level*0.5).toInt()
+            level++
+            binding.levelTextView.text = "Level: $level"
+        }
+    }
+    public fun setTABBinding(binding: ActivityMainBinding) {
+        this.binding = binding
+    }
 }
